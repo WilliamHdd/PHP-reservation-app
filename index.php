5f71212b31a4ca7001d01038b45accc47d30b032
Mathieu David <mathieudavid@mathieudavid.org>
2016-10-14 10:17:17 +0200

    Added bootstrap and JQuery as a dependency to the project

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
         <meta charset="utf-8">
         <title>Réservation</title>
         <link rel="stylesheet" href="css/style.css">
+
+        <!-- Load bootstrap 4 stylesheets and dependencies -->
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/tether/dist/css/tether.min.css">
+        <script src="node_modules/jquery/dist/jquery.min.js" charset="utf-8"></script>
     </head>
     <body>
         <h1>Réservation</h1>
+
+        <script src="node_modules/tether/dist/js/tether.min.js" charset="utf-8"></script>
     </body>
 </html>
