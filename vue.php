4afedad54dc3a4aee4e43133a9b6dc88100be195
WilliamHdd <williamhdd@yahoo.fr>
2016-10-14 13:20:32 +0200

    Page 2 ready + End page ready

--- vue.php
+++ vue.php
@@ -1,22 +1,51 @@
 <!DOCTYPE html>
 <html>
+
     <head>
-      Détails de réservation:
+        <meta charset="utf-8">
+        <title>Détails de réservation:</title>
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Load bootstrap 4 stylesheets and dependencies -->
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/tether/dist/css/tether.min.css">
+        <script src="node_modules/jquery/dist/jquery.min.js" charset="utf-8"></script>
+
     </head>
     <body>
-      <p> Voyage vers <?php echo $_session["destination"]; ?> </p>
-      <?php for ($i = (int)$_POST['places'] ;$i > 0; $i --)
+      <h1>Détails de réservation:</h1>
+      <p> Voyage vers <?php echo $_session["destination"],":"; ?> </p>
+      <?php
+      if(isset($_session['assurance']))
       {
-        echo "Voyageur" ;
+        echo "Avec assurance annulation.<br><br>";
+      }
+      else
+      {
+        echo "Sans assurance annulation.<br><br>";
+      }
+
+      ?>
+      <form method="post" action="step_2.php">
+      <?php for ($i = 1  ;$i < (int)$_POST['places'] + 1; $i ++)
+      {
+        echo "Voyageur ", $i;
         //ini_set('display_errors', 1);
         //ini_set('display_startup_errors', 1);
         //error_reporting(E_ALL);
         ?>
-      <form method="post">
+
         <p> Nom: <input type="text" placeholder="Voyageur " /></p>
         <p> Age: <input type="text" placeholder="Age" /></p>
-      </form>
       <?php } ?>
-      <input type="button" value="Clic !" /><input type="button" value="Clic !" /><input type="button" value="Clic !" />
-    </body>
+      <p>
+        <button name="step_2" class="btn btn-primary btn-lg" name = "Submit">Suivant</button>
+        <button name="destroy_2" class="btn btn-default btn-lg">Annulation</button>
+      </p>
+      </form>
+
+      <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
+      <script src="node_modules/tether/dist/js/tether.min.js" charset="utf-8"></script>
+
+      </body>
 </html>
