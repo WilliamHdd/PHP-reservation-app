51e4c9d80855ab8bdd746e23a10a38ff39480571
WilliamHdd <williamhdd@yahoo.fr>
2016-10-18 18:38:32 +0200

    further test and dic, still not working

--- step_2.php
+++ step_2.php
@@ -4,6 +4,7 @@ if (isset($_POST['step_2']))
     //ini_set('display_errors', 1);
     //ini_set('display_startup_errors', 1);
     //error_reporting(E_ALL);
+    //$_session['people'] = $_POST['people'];
     include("end.php");
   }
   elseif (isset($_POST['destroy_2']))
