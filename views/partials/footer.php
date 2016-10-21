791b0e08f7a40da874498ce7086318e39a58351e
Mathieu David <mathieudavid@mathieudavid.org>
2016-10-21 09:41:05 +0200

    start reorganizing files

---
+++ views/partials/footer.php
@@ -0,0 +1,7 @@
+</div>
+
+  <script src="node_modules/tether/dist/js/tether.min.js" charset="utf-8"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
+</body>
+
+</html>
---
+++ views/partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Réservation</title>
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Load bootstrap 4 stylesheets and dependencies -->
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/tether/dist/css/tether.min.css">
+        <script src="node_modules/jquery/dist/jquery.min.js" charset="utf-8"></script>
+    </head>
+    <body>
+      <div class="container">
